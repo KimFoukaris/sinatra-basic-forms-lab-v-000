306ef122887ba9b7686cdb2cd0758014270598c7
Kim Foukaris <kimfoukaris@outlook.com>
2018-03-31 02:49:25 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,4 +2,20 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+get '/' do
+  erb :index
+end
+
+get '/new' do
+  erb :create_puppy
+end
+
+post '/new' do
+  puppy_name = params[:name]
+  puppy_breed = params[:breed]
+  puppy_age = params[:age]
+  @puppy = Puppy(name, age, breed)
+  erb :display_puppy
+end
+
 end
